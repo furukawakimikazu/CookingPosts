5ff1a543e548b0645a92a0c0835a48eeb3d5df15
furukawakimikazu <g.guquan@gmail.com>
2022-04-26 14:43:14 +0900

    refile_keyの追加config/initializers/application_controller_renderer.rb

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '14b1157ac425eb31be11ab8dd8b5511088f21c16a5caee3cf3eb649d0054f7c23e5e4cc511877910cf4c790f9ff5a7082dab7c6d984ee17a6e746d52a8caefae'
+
